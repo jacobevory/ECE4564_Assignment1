737f532db017e04a5ee7bc4b8c9759e1b2094bae
jacobevory <jacobevory@gmail.com>
2017-10-03 16:33:51 -0400

    Update clientKeys.py

---
+++ clientKeys.py
@@ -0,0 +1,9 @@
+#keys to access twitter
+
+ckey = '81bWC84J6xGLmjDyPdrChhz02'
+csec = 'crs5WnkfMokqo5vjG7ceWuAqIqEuzgBBxCzTKczNmtEdCGcI5Y'
+atok = '913827757070733312-qHrxDoaavKU8s9Ww2L2PG6XTE9JDvYg'
+asec = 'JM1HOhEpvvz5In4UtXa7HxumMfFc348q6wZJzPFwS3Jp0'
+
+track = ['#ECE4564T26']
+hashstr = '#ECE4564T26'
